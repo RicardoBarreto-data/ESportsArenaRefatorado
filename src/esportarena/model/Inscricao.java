7edcafe1c162ce20cc65eb095e87fbb0ed941295
Ricardo Barreto <ricardobarreto687@gmail.com>
2025-11-18 15:33:14 -0300

    Adicionar entidades do domínio (Usuario, Torneio, Time, Jogador, Partida, Ranking, Inscricao) - modelo inicial

--- src/esportarena/model/Inscricao.java
+++ src/esportarena/model/Inscricao.java
@@ -1,13 +1,45 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package esportarena.model;
 
-/**
- *
- * @author Micro
- */
+import java.time.LocalDateTime;
+import java.util.Objects;
+
 public class Inscricao {
     
+private int idInscricao;
+    private int idTorneio;
+    private int idJogador;
+    private LocalDateTime dataInscricao;
+
+    public Inscricao() {}
+
+    public Inscricao(int idInscricao, int idTorneio, int idJogador, LocalDateTime dataInscricao) {
+        this.idInscricao = idInscricao;
+        this.idTorneio = idTorneio;
+        this.idJogador = idJogador;
+        this.dataInscricao = dataInscricao;
+    }
+
+    public int getIdInscricao() { return idInscricao; }
+    public void setIdInscricao(int idInscricao) { this.idInscricao = idInscricao; }
+
+    public int getIdTorneio() { return idTorneio; }
+    public void setIdTorneio(int idTorneio) { this.idTorneio = idTorneio; }
+
+    public int getIdJogador() { return idJogador; }
+    public void setIdJogador(int idJogador) { this.idJogador = idJogador; }
+
+    public LocalDateTime getDataInscricao() { return dataInscricao; }
+    public void setDataInscricao(LocalDateTime dataInscricao) { this.dataInscricao = dataInscricao; }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Inscricao)) return false;
+        Inscricao inscricao = (Inscricao) o;
+        return idInscricao == inscricao.idInscricao;
+    }
+
+    @Override
+    public int hashCode() { return Objects.hash(idInscricao);
+  }
 }
